6aeadde7bc0544d64cd0679dfb021b9ef963ca98
Dinar <gabbasov@it-projects.info>
2016-01-22 21:59:33 +0500

    added func
    add new function

--- chess/js/chess.js
+++ chess/js/chess.js
@@ -1257,9 +1257,8 @@ window['Chess'] = window['Chess'] || function(fen) {
       var pgn_move_number = 1;
 
       /* build the list of moves.  a move_string looks like: "3. e3 e6" */
-      while (reversed_history.length > 0) {
+	    while (reversed_history.length > 0) {
         var move = reversed_history.pop();
-
         /* if the position started with black to move, start PGN with 1. ... */
         if (pgn_move_number === 1 && move.color === 'b') {
           move_string = '1. ...';
@@ -1269,14 +1268,16 @@ window['Chess'] = window['Chess'] || function(fen) {
           if (move_string.length) {
             moves.push(move_string);
           }
-          move_string = pgn_move_number + '.';
+          var dt_move = [make_pretty(move).from + '-' + make_pretty(move).to];
+          var temp = reversed_history.slice(-1);
+		  move_string = pgn_move_number + '.';
           pgn_move_number++;
         }
-
-        move_string = move_string + ' ' + move_to_san(move);
+        move_string = move_string + ' ' +'<a href="#" data-move="'+dt_move+'">' + move_to_san(move) + '</a>';
+		dt_move.pop();
+		if(temp.length) dt_move.push(make_pretty(temp[0]).from + '-' + make_pretty(temp[0]).to);
         make_move(move);
       }
-
       /* are there any other leftover moves? */
       if (move_string.length) {
         moves.push(move_string);
